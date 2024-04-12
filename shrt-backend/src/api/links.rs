b2bfab80f8d590b90225b979ca0368f6ce5a7a63
Mateusz Maćkowski <m4tx@m4tx.pl>
2024-04-12 14:57:14 +0200

    feat(api): use Base56 alphabet for the generated slugs

--- shrt-backend/src/api/links.rs
+++ shrt-backend/src/api/links.rs
@@ -161,9 +161,9 @@ pub async fn get_links(
 }
 
 /// The alphabet to generate the links from. Essentially [0-9A-Za-z], but with
-/// '1', 'i', 'l', 'I', 'o', 'O' removed to avoid confusion.
+/// '0', 'o', 'O', '1', 'l', 'I' removed to avoid confusion (Base56 alphabet).
 const ALPHABET: [char; 56] = [
-    '0', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k',
+    '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F',
     'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
 ];
