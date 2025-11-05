154d4ade6628597bb800997401d37b0b8ea03f50
Mateusz Maćkowski <m4tx@m4tx.pl>
2025-11-05 20:35:01 +0100

    chore: fix clippy warnings

--- shrt-frontend/src/main.rs
+++ shrt-frontend/src/main.rs
@@ -1,14 +1,12 @@
 mod api;
 mod app;
 mod error_alert;
-mod hand_example;
 mod input;
 mod link_result;
 mod list_links;
 mod not_found;
 mod pagination;
 mod remove_link_modal;
-mod select;
 mod url_shortener;
 
 use app::App;
